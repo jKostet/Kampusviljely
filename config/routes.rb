57f695aa5359064368720dfd687da89b733e5bc6
jKostet <juho.kostet@helsinki.fi>
2017-01-07 14:03:43 +0200

    rails g StaticPages home help about

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
+  get 'static_pages/about'
+
   resources :bags
   resources :sites
   # The priority is based upon order of creation: first created -> highest priority.
